e352ab82d79cdd3f64f3c67b5b6ef063517dc493
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-27 10:59:22 +0300

    [:setup] Build | Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
-    version="1.0.1",
+    version="1.0.2",
     entry_points="""
         [console_scripts]
         gico = gico.gico:main
